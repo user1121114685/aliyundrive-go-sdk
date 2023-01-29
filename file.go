186392f56ca16737148c2cd4379c150dd8ae436a
B <b@doubi.fun>
2023-01-29 17:17:57 +0800

    增加ReadDir支持

--- file.go
+++ file.go
@@ -15,6 +15,7 @@ const KB = 1024
 const MB = 1024 * KB
 const GB = 1024 * MB
 const TB = 1024 * GB
+const LimitMax = 200
 
 const OrderDirectionDesc = "DESC"
 const OrderDirectionAsc = "ASC"
--- fs/fs.go
+++ fs/fs.go
@@ -22,47 +22,60 @@ func New(c *aliyundrive.Drive, root string) fs.FS {
 }
 
 func (f *Fs) Open(name string) (fs.File, error) {
-	item, err := f.pathToItem(name)
+	return f.open(context.Background(), name)
+}
+
+func (f *Fs) ReadDir(name string) ([]fs.DirEntry, error) {
+	file, err := f.open(context.Background(), name)
 	if err != nil {
 		return nil, err
 	}
-	return &File{fs: f, item: item}, nil
+	defer file.close()
+	return file.ReadDir(-1)
 }
 
-func (f *Fs) pathToItem(p string) (item *aliyundrive.Item, err error) {
+func (f *Fs) open(ctx context.Context, p string) (*File, error) {
+	root, err := f.c.DoGetRequest(ctx,
+		aliyundrive.GetRequest{FileId: aliyundrive.RootFileId})
+	if err != nil {
+		return nil, err
+	}
+
 	p = path.Join(f.root, p)
 	paths := splitPath(p)
+	file := &File{fs: f, item: &root.Item}
 
-	ctx := context.Background()
-	parentFileId := "root"
-	exists := false
 	for _, name := range paths {
 		if name == "/" {
-			parentFileId = "root"
 			continue
 		}
-		nextmarker := ""
+
+		next := ""
+		var targetItem *aliyundrive.Item
 		for {
-			resp, err := f.c.DoListRequest(ctx, aliyundrive.ListRequest{
-				ParentFileId: parentFileId,
-				Limit:        200,
-				NextMarker:   nextmarker,
-			})
+			var items []*aliyundrive.Item
+			items, next, err = file.list(ctx, aliyundrive.LimitMax, next)
 			if err != nil {
 				return nil, err
 			}
-			item, exists = aliyundrive.ItemQuery(resp.Items).ByName(name)
-			if exists || resp.NextMarker == "" {
+			for _, item := range items {
+				if item.Name == name {
+					targetItem = item
+					break
+				}
+			}
+			if targetItem != nil || next == "" {
 				break
 			}
-			nextmarker = resp.NextMarker
 		}
-		if !exists {
+
+		if targetItem == nil {
 			return nil, fs.ErrNotExist
 		}
-		parentFileId = item.FileId
+		file = &File{fs: f, item: targetItem}
 	}
-	return
+
+	return file, nil
 }
 
 type File struct {
@@ -87,12 +100,20 @@ func (f *File) Mode() fs.FileMode {
 	return 0
 }
 
+func (f *File) Type() fs.FileMode {
+	return f.Mode()
+}
+
 func (f *File) ModTime() time.Time {
 	return f.item.UpdatedAt
 }
 
 func (f *File) IsDir() bool {
 	return f.item.Type == "folder"
+}
+
+func (f *File) Info() (fs.FileInfo, error) {
+	return f, nil
 }
 
 func (f *File) Sys() any {
@@ -105,7 +126,8 @@ func (f *File) Stat() (fs.FileInfo, error) {
 func (f *File) Read(p []byte) (int, error) {
 	// 第一次读，初始化
 	if f.body == nil {
-		if err := f.prepareReader(0); err != nil {
+		err := f.prepareReader(context.Background(), 0)
+		if err != nil {
 			return 0, err
 		}
 	}
@@ -124,19 +146,40 @@ func (f *File) Seek(offset int64, whence int) (int64, error) {
 	}
 
 	f.close()
-	if err := f.prepareReader(offset); err != nil {
+	err := f.prepareReader(context.Background(), offset)
+	if err != nil {
 		return 0, err
 	}
 	return offset, nil
+}
+
+func (f *File) ReadDir(n int) (entries []fs.DirEntry, err error) {
+	if n > 1 {
+		return nil, fs.ErrInvalid
+	}
+	ctx := context.Background()
+	next := ""
+	var items []*aliyundrive.Item
+	for {
+		items, next, err = f.list(ctx, aliyundrive.LimitMax, next)
+		if err != nil {
+			return
+		}
+		for _, item := range items {
+			entries = append(entries, &File{fs: f.fs, item: item})
+		}
+		if next == "" {
+			break
+		}
+	}
+	return
 }
 
 func (f *File) Close() error {
 	return f.close()
 }
 
-func (f *File) prepareReader(offset int64) error {
-	ctx := context.Background()
-
+func (f *File) prepareReader(ctx context.Context, offset int64) error {
 	// 获取下载链接
 	getDownloadUrlResp, err := f.fs.c.DoGetDownloadUrlRequest(ctx, aliyundrive.GetDownloadUrlRequest{
 		FileId: f.item.FileId,
@@ -176,6 +219,27 @@ func (f *File) close() error {
 	f.body = nil
 	f.offset = 0
 	return err
+}
+
+func (f *File) list(ctx context.Context, limit int, next string) (items []*aliyundrive.Item, nextMarker string, err error) {
+	if !f.IsDir() {
+		err = fs.ErrInvalid
+		return
+	}
+
+	resp, err := f.fs.c.DoListRequest(ctx, aliyundrive.ListRequest{
+		ParentFileId:   f.item.FileId,
+		OrderBy:        aliyundrive.OrderByName,
+		OrderDirection: aliyundrive.OrderDirectionAsc,
+		Limit:          limit,
+		NextMarker:     next,
+	})
+	if err != nil {
+		return
+	}
+	nextMarker = resp.NextMarker
+	items = resp.Items
+	return
 }
 
 func splitPath(p string) []string {
